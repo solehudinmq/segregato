fae7096d6037a96e087b325aec8c386ee0723623
SolehMQ <solehudinmq@gmail.com>
2025-10-10 17:04:42 +0700

    first commit

---
+++ lib/segregato.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "segregato/version"
+
+module Segregato
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/segregato/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Segregato
+  VERSION = "0.1.0"
+end
